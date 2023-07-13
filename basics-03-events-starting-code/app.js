6a56d603c5f767a29790a1f7c6d60a286292a325
Vitaliy Semenov <synthex@bk.ru>
2023-07-13 16:53:48 +0600

    feat: locking content with v-once

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: '',
     };
   },
+  methods: {
+    add(num) {
+      this.counter += num;
+    },
+    subtract(num) {
+      this.counter -= num;
+    },
+    setName(event, lastname) {
+      this.name = `${event.target.value} ${lastname}`;
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('Submitted!');
+    }
+  }
 });
 
 app.mount('#events');
