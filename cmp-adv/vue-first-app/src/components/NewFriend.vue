f985052316ef943950a10d24b0581268f3b36c07
Vitaliy Semenov <synthex@bk.ru>
2023-07-17 09:30:41 +0600

    cmp-adv

---
+++ cmp-adv/vue-first-app/src/components/NewFriend.vue
@@ -0,0 +1,67 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label
+        >Name
+        <input type="text" v-model="enteredName" />
+      </label>
+    </div>
+    <div>
+      <label>Phone <input type="tel" v-model="enteredPhone" /></label>
+    </div>
+    <div>
+      <label>Email <input type="email" v-model="enteredEmail" /></label>
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['add-contact'],
+  data() {
+    return {
+      enteredName: '',
+      enteredPhone: '',
+      enteredEmail: ''
+    }
+  },
+  methods: {
+    submitData() {
+      this.$emit('add-contact', this.enteredName, this.enteredPhone, this.enteredEmail)
+      this.enteredName = ''
+      this.enteredPhone = ''
+      this.enteredEmail = ''
+    }
+  }
+}
+</script>
+
+<style>
+form {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+}
+
+form div {
+  margin: 1rem 0;
+}
+</style>
--- cmp-adv/vue-first-app/src/main.js
+++ cmp-adv/vue-first-app/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import App from './App.vue'
+import FriendContact from './components/FriendContact.vue';
+import NewFriend from './components/NewFriend.vue';
 
+const app = createApp(App);
+app.component('friend-contact', FriendContact);
+app.component('new-friend', NewFriend);
 
-const app = createApp({})
-app.mount('#app')
+app.mount('#app');
